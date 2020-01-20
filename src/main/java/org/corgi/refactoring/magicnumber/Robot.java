1710569b2e1cf337432d6cea4862819d37a84ff9
ksw6169 <ksw6169@naver.com>
2020-01-20 22:54:31 +0900

    1. 매직 넘버를 기호 상수로 치환

--- src/main/java/org/corgi/refactoring/magicnumber/Robot.java
+++ src/main/java/org/corgi/refactoring/magicnumber/Robot.java
@@ -4,17 +4,20 @@ public class Robot {
 
     private final String name;
 
+    public static final RobotCommand COMMAND_WALK = new RobotCommand("WALK");
+    public static final RobotCommand COMMAND_STOP = new RobotCommand("STOP");
+    public static final RobotCommand COMMAND_JUMP = new RobotCommand("JUMP");
+
     public Robot(String name) {
         this.name = name;
     }
 
-    public void order(int command) {
-
-        if (command == 0) {
+    public void order(RobotCommand command) {
+        if (command == COMMAND_WALK) {
             System.out.println(name+" walks.");
-        } else if (command == 1) {
+        } else if (command == COMMAND_STOP) {
             System.out.println(name+" stops.");
-        } else if (command == 2) {
+        } else if (command == COMMAND_JUMP) {
             System.out.println(name+" jumps");
         } else {
             System.out.println("Command error. command = "+command);
