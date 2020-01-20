d282de845f8fb1a3a1d52106ea47fba539a5d41b
ksw6169 <ksw6169@naver.com>
2020-01-20 21:54:54 +0900

    1. 매직 넘버를 기호 상수로 치환

---
+++ src/main/java/org/corgi/refactoring/magicnumber/Main.java
@@ -0,0 +1,12 @@
+package org.corgi.refactoring.magicnumber;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Robot robot = new Robot("Andrew");
+        robot.order(0);
+        robot.order(1);
+        robot.order(2);
+    }
+}
