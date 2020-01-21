29239640ab00786b7e50fa989835b368a846c54e
ksw6169 <ksw6169@naver.com>
2020-01-22 00:30:01 +0900

    4. Null 객체 도입

---
+++ src/main/java/org/corgi/refactoring/nullobject/Main.java
@@ -0,0 +1,19 @@
+package org.corgi.refactoring.nullobject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(new Label("Alice"), new Label("alice@example.com")));
+        people.add(new Person(new Label("Bobby"), new Label("bobby@example.com")));
+        people.add(new Person(new Label("Chris")));
+
+        for (Person person : people) {
+            System.out.println(person.toString());
+        }
+    }
+}
