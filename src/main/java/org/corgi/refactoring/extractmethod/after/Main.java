fbf65f939cc5f43c8ec4b444c782a97a98ba1e06
ksw6169 <ksw6169@naver.com>
2020-02-08 22:31:26 +0900

    5. 메서드 추출

---
+++ src/main/java/org/corgi/refactoring/extractmethod/after/Main.java
@@ -0,0 +1,10 @@
+package org.corgi.refactoring.extractmethod.after;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Banner hello = new Banner("Hello, World!");
+        hello.print(3);
+    }
+}
---
+++ src/main/java/org/corgi/refactoring/extractmethod/before/Banner.java
@@ -0,0 +1,36 @@
+package org.corgi.refactoring.extractmethod.before;
+
+public class Banner {
+
+    private final String content;
+
+    public Banner(String content) {
+        this.content = content;
+    }
+
+    public void print(int times) {
+
+        // 테두리 출력
+        System.out.print("+");
+
+        for (int i=0; i<content.length(); i++) {
+            System.out.print("-");
+        }
+
+        System.out.println("+");
+
+        // 내용 출력
+        for (int i=0; i<times; i++) {
+            System.out.println("|"+content+"|");
+        }
+
+        // 테두리 출력
+        System.out.print("+");
+
+        for (int i=0; i<content.length(); i++) {
+            System.out.print("-");
+        }
+
+        System.out.println("+");
+    }
+}
