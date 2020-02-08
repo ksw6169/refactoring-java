24f7b3dbe6797ae6fc4145ef766c78c22fee7b26
ksw6169 <ksw6169@naver.com>
2020-02-08 23:57:25 +0900

    8. 분류 코드를 하위 클래스로 치환

---
+++ src/main/java/org/corgi/refactoring/chapter8/replacetypecodewithsubclasses/after/Main.java
@@ -0,0 +1,20 @@
+package org.corgi.refactoring.chapter8.replacetypecodewithsubclasses.after;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Shape line = Shape.createShape(Shape.TYPECODE_LINE, 0,0,100,200);
+        Shape rectangle = Shape.createShape(Shape.TYPECODE_RECTANGLE, 10,20,30,40);
+        Shape oval = Shape.createShape(Shape.TYPECODE_OVAL, 100,200,300,400);
+
+        Shape[] shape = {
+            line, rectangle, oval
+        };
+
+        Arrays.asList(line, rectangle, oval)
+                .forEach(s -> s.draw());
+    }
+}
